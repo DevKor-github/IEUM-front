2950369430e7254da90838a578d699f3a9e0a16e
jeongmin hwang <jeongmin010324@gmail.com>
2024-03-19 18:17:47 +0900

    feat : ts 변경 대응

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Main = () => {
+
+    return (
+        <React.Fragment>
+            <div>
+                <span>Main Page</span>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Main;
