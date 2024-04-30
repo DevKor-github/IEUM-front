7f852389508f2d52abebd6786d0c2e01c3dc71d7
anna <imannamj@gmail.com>
2024-04-30 17:41:40 +0900

    Feat: SpotList 무한 스크롤 구현

--- src/components/sidebar/SpotList.tsx
+++ src/components/sidebar/SpotList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Area_Select from '../../assets/images/area_select.svg';
 import '../../assets/styles/spot.css';
 import { useAppSelector } from '../../redux/hook';
@@ -6,22 +6,43 @@ import { getSelectedSpotProps, getSpotListProps } from '../../redux/spotSlice';
 import SpotInfo from './SpotInfo';
 import SpotDetailInfo from './SpotDetailInfo';
 import Dropdown from './Dropdown';
+import { SpotType } from '../../types/map.type';
 
 const SpotList = () => {
   const [view, setView] = useState(false);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [spots, setSpots] = useState<SpotType[]>([]);
 
   const spotListState = useAppSelector(getSpotListProps);
   const selectedSpotState = useAppSelector(getSelectedSpotProps);
 
-  const setSpotInfo = () => {
-    return (
-      <>
-        {spotListState?.map((spot, idx) => (
-          <SpotInfo key={idx} spotType={spot} />
-        ))}
-      </>
-    );
+  useEffect(() => {
+    if (!loading) {
+      setLoading(true);
+      // 아이템 로드 로직 추가 (API 호출?)
+      // setSpots 함수로 spots 상태에 추가
+    }
+  }, [page]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setPage((prevPage) => prevPage + 1);
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    setSpots((prevSpots: SpotType[]) => [...prevSpots, ...spotListState]);
+    setLoading(false);
+  }, [spotListState]);
 
   return (
     <div className="spot-container">
@@ -45,7 +66,12 @@ const SpotList = () => {
       <hr />
 
       {!selectedSpotState ? (
-        <div className="spot-list">{setSpotInfo()}</div>
+        <div className="spot-list">
+          {spots.map((spot, idx) => (
+            <SpotInfo key={idx} spotType={spot} />
+          ))}
+          {loading && <p>Loading...</p>}
+        </div>
       ) : (
         <div className="spot-list">
           <SpotDetailInfo spotContent={selectedSpotState} />
