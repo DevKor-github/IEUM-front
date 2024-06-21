e8dce0b94c6d905778ae580b4c227675b8df5d9d
jeongmin <jeongmin010324@gmail.com>
2024-06-21 19:37:31 +0900

    FEAT : 무한 스크롤 개선

--- src/components/sidebar/SpotList.tsx
+++ src/components/sidebar/SpotList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Area_Select from '../../assets/images/area_select.svg';
 import '../../assets/styles/spot.css';
 import { useAppDispatch, useAppSelector } from '../../redux/hook';
@@ -14,71 +14,58 @@ import Dropdown from './Dropdown';
 import { SpotType } from '../../types/map.type';
 import { getUserCollectionList } from '../../api/instagram';
 import { useParams } from 'react-router-dom';
+import { useIntersectionObserver } from '../UseIntersectionObserver';
 
 const SpotList = () => {
   const dispatch = useAppDispatch();
 
   const params = useParams();
-  const [view, setView] = useState(false);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
   const [spots, setSpots] = useState<SpotType[]>([]);
-  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
-  const [nextCursorId, setNextCursorId] = useState<number>(0);
-  const spotListState = useAppSelector(getSpotListProps);
   const selectedSpotIdState = useAppSelector(getSelectedSpotIdProps);
+  const lastElementRef = useRef<HTMLDivElement | null>(null);
+  const nextCursorId = useRef<number>(0);
+  const hasNextPage = useRef<boolean>(true);
+  const loading = useRef<boolean>(false);
+
+  const { observe, unobserve } = useIntersectionObserver({
+    onIntersection({ target }) {
+      getNextCollectionList();
+    },
+  });
 
   useEffect(() => {
-    // 초기 랜더링
-    if (spotListState.length == 0) {
-      getNextCollectionList();
-    }
+    const lastElement = lastElementRef?.current;
+    if (!lastElement) return;
+    if (loading.current) return;
+    observe(lastElement);
+    return () => {
+      unobserve(lastElement);
+    };
   }, []);
-
-  useEffect(() => {
-    if (!loading && hasNextPage && page > 1) {
-      setLoading(true);
-      // 아이템 로드 로직 추가 (API 호출?)
-      // setSpots 함수로 spots 상태에 추가
-      getNextCollectionList();
-    }
-  }, [page]);
 
   const getNextCollectionList = async () => {
     try {
+      if (!hasNextPage.current && loading.current) return;
+      loading.current = true;
       const res = await getUserCollectionList(
         params.userId || '',
-        nextCursorId,
+        nextCursorId.current,
       );
-      dispatch(setSpotList(res.spotData));
-      setHasNextPage(res?.hasNextPage);
-      setNextCursorId(res?.nextCursorId);
+      setSpots((prevSpots: SpotType[]) => [...prevSpots, ...res?.spotData]);
+      hasNextPage.current = res?.hasNextPage;
+      nextCursorId.current = res?.nextCursorId;
     } catch (err) {
       dispatch(setIsValidUser(false));
     }
-  };
-
-  const handleScroll = () => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop !==
-      document.documentElement.offsetHeight
-    )
-      return;
-    setPage((prevPage) => prevPage + 1);
+    loading.current = false;
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  useEffect(() => {
-    setSpots((prevSpots: SpotType[]) => [...prevSpots, ...spotListState]);
-    setLoading(false);
-  }, [spotListState]);
+    dispatch(setSpotList(spots));
+  }, [spots]);
 
   return (
-    <div className="spot-container">
+    <div className="spot-container" id="spot-container">
       {/*<div className="category">*/}
       {/*  <div className="header">*/}
       {/*    <div className="dropdown">*/}
@@ -103,7 +90,13 @@ const SpotList = () => {
           {spots.map((spot, idx) => (
             <SpotInfo key={idx} spotType={spot} />
           ))}
-          {loading && <p>Loading...</p>}
+
+          {!loading.current && (
+            <p
+              ref={lastElementRef}
+              style={{ height: '10px', margin: '0px' }}
+            ></p>
+          )}
         </div>
       ) : (
         <div className="spot-list">
