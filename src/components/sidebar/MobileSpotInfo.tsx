8654bacbf9b9173a298df0eaa0f2ac3ed12cfd79
anna <imannamj@gmail.com>
2024-07-03 03:54:31 +0900

    모바일뷰 qa 목록 사항 변경

--- src/components/sidebar/MobileSpotInfo.tsx
+++ src/components/sidebar/MobileSpotInfo.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { SpotType } from '../../types/map.type';
+import { setSelectedSpotId } from '../../redux/spotSlice';
+import { useAppDispatch } from '../../redux/hook';
 
 interface SpotInfoPropsType {
   spotType: SpotType;
@@ -8,6 +10,8 @@ interface SpotInfoPropsType {
 const MobileSpotInfo = (props: SpotInfoPropsType) => {
   const { spotContent, position, icon } = props.spotType;
   const [instaContent, setInstaContent] = useState<string>('');
+  const dispatch = useAppDispatch();
+  // const [moreInfo, setMoreInfo] = useState(false);
 
   useEffect(() => {
     if (window?.instgrm) {
@@ -17,14 +21,41 @@ const MobileSpotInfo = (props: SpotInfoPropsType) => {
     const doc = parser.parseFromString(spotContent.embeddedTag, 'text/html');
     setInstaContent(doc.documentElement.textContent || '');
   }, [instaContent]);
+
+  const handleSpot = (id: number) => {
+    dispatch(setSelectedSpotId(id));
+  };
+
   return (
     <>
-      <div className="spot-info">
+      <div
+        className="spot-info"
+        onClick={() => {
+          handleSpot(spotContent.instaGuestCollectionId);
+        }}
+      >
         <div>
           <span className="spot-name">{spotContent.placeName}</span>
           <span className="spot-type">{spotContent.category}</span>
         </div>
         <span className="address">{spotContent.address}</span>
+        {/* {!moreInfo ? (
+          <div className="content summary">
+            <span className="hashtag">{spotContent.tags?.join(',')}</span>
+            <p>{spotContent.instagramDescription}</p>
+            <button
+              className="info button"
+              onClick={() => setMoreInfo(!moreInfo)}
+            >
+              더보기
+            </button>
+          </div>
+        ) : (
+          <div className="content summary">
+            <span className="hashtag">{spotContent.tags?.join(',')}</span>
+            <p>{spotContent.instagramDescription}</p>
+          </div>
+        )} */}
         <div className="content summary">
           <span className="hashtag">{spotContent.tags?.join(',')}</span>
           <p>{spotContent.instagramDescription}</p>
@@ -34,6 +65,7 @@ const MobileSpotInfo = (props: SpotInfoPropsType) => {
         className="imbed"
         dangerouslySetInnerHTML={{ __html: instaContent }}
       ></div>
+      <div style={{ height: '45px' }}></div>
     </>
   );
 };
